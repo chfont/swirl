3aa0709adc9692eb3b695da9c11d411e9e2c81ba
mango <fontenot.christian@outlook.com>
2022-11-29 14:14:29 -0700

    Add ability to read formula from file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from lib.grammar import *
 from lib.automaton import *
 
-prog = input("Enter a formula: ")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f","--file",type=str, help="File to read formula from")
+
+
+args = parser.parse_args()
+if args.file is not None:
+    prog = open(args.file, "r").read()
+else:
+    prog = input("Enter a formula: ")
 tree = s1s_parse(prog)
 aut = ast_to_automaton(tree)
 
